b74fed0f35c5fcb8c6c9413aaa8290c155156638
Steven Nguyen <stevennguyen901@hotmail.com>
2020-12-09 22:20:40 -0800

    add function that blurs image if user enters blur into input box and presses submit

--- JS/app.js
+++ JS/app.js
@@ -10,6 +10,10 @@ const thumbDiv = document.querySelector('#thumb-bar')
 
 const displayedImg = document.querySelector('.displayed-img')
 
+const footerElement = document.querySelector('footer')
+
+const userInput = document.querySelector('input')
+
 for (item of imgArray) {
     console.log(item.pic)
     const insertPic = `
@@ -27,6 +31,37 @@ function displayPic(item, array) {
             attribute.value = `images/${imageString}`
             console.log(attribute)
             displayedImg.setAttributeNode(attribute)
+            const insertInput = `
+        <label>What effect do you want to apply?</label>
+        <input id="effect" type="text">
+        <button onclick='submitBtn(displayedImg, imgArray)'>Submit</button>
+        `
+        if (footerElement.childElementCount == 0) {
+        footerElement.insertAdjacentHTML('afterbegin', insertInput)
+            }
         }
     }
+}
+
+function submitBtn(tag, array) {
+    const inputValue = document.querySelector('input').value
+    const userInput = document.querySelector('input')
+    const tagSource = tag.src
+    console.log(tagSource)
+    console.log(array)
+    if (inputValue == "blur") {
+        for (item of array) {
+            const imageString = item.pic
+            if (tagSource.includes(imageString)) {
+                const attribute = document.createAttribute("src")
+                const blurImg = item.blur
+                console.log(blurImg)
+                attribute.value = `images/${blurImg}`
+                displayedImg.setAttributeNode(attribute)
+                }
+            }
+        } else {
+            alert("Invalid input")
+            }   
+    userInput.value = ''
 }
